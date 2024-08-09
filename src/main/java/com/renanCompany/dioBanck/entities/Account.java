df0a8c45b2e3fc4e8bbd7e048ab822da0f64f3f7
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 12:22:46 -0300

    Criation of class CheckingAccount

--- src/main/java/com/renanCompany/dioBanck/entities/Account.java
+++ src/main/java/com/renanCompany/dioBanck/entities/Account.java
@@ -1,10 +1,41 @@
 package com.renanCompany.dioBanck.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.renanCompany.dioBanck.enums.AccountStatus;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "tb_account")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private Long agencyNumber;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private AccountStatus status;
 
 }
