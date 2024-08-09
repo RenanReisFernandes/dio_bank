b40211f4e59c3f230b13a133203aa62a06c1c8ae
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 12:00:12 -0300

    Criation of entitie Account

---
+++ src/main/java/com/renanCompany/dioBanck/entities/Account.java
@@ -0,0 +1,10 @@
+package com.renanCompany.dioBanck.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_account")
+public class Account {
+
+}
