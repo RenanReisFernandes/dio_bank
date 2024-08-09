1273a2bf27843384b27971239ce7f750ba63fce8
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 11:12:24 -0300

    initial commit

---
+++ src/main/java/com/renanCompany/dioBanck/DioBanckApplication.java
@@ -0,0 +1,13 @@
+package com.renanCompany.dioBanck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBanckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBanckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanCompany/dioBanck/DioBanckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanCompany.dioBanck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBanckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
