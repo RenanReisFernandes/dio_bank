5b81ae66e213cb406f9f6ab07dc5ec2a26342460
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 12:52:16 -0300

    Criation of relationships

--- src/main/java/com/renanCompany/dioBanck/entities/Account.java
+++ src/main/java/com/renanCompany/dioBanck/entities/Account.java
@@ -1,5 +1,7 @@
 package com.renanCompany.dioBanck.entities;
 
+import java.math.BigDecimal;
+
 import com.renanCompany.dioBanck.enums.AccountStatus;
 
 import jakarta.persistence.Column;
@@ -11,6 +13,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -33,6 +37,11 @@ public class Account {
 	private Long id;
 	@Column(nullable = false)
 	private Long agencyNumber;
+	private BigDecimal balance;
+	
+    @OneToOne(optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
+	private Client client;
 	
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
