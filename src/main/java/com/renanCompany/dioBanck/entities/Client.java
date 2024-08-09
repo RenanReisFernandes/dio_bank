20eb3cca1728ebe7a022e171b63b3a072c88a111
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 11:41:00 -0300

    Client class criation

--- src/main/java/com/renanCompany/dioBanck/entities/Client.java
+++ src/main/java/com/renanCompany/dioBanck/entities/Client.java
@@ -1,4 +1,29 @@
 package com.renanCompany.dioBanck.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_client")
 public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private String address;
 }
