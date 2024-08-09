c7726e3bab6707a0a54a77f3599cfe5fab633da6
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-09 15:40:46 -0300

    creating method save Client

--- src/main/java/com/renanCompany/dioBanck/services/ClientService.java
+++ src/main/java/com/renanCompany/dioBanck/services/ClientService.java
@@ -1,8 +1,28 @@
 package com.renanCompany.dioBanck.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.renanCompany.dioBanck.entities.Client;
+import com.renanCompany.dioBanck.repositories.ClientRepository;
 
 @Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	public Client save(Client client) {
+		Optional<Client> optClient = clientRepository.findByCpf(client.getCpf());
+		
+		if(optClient.isPresent()) {
+			if(!optClient.get().getId().equals(client.getId())) {
+				throw new RuntimeException("Cliente já cadastrado!");
+			}
+		}
+		return client;
+	}
 
 }
