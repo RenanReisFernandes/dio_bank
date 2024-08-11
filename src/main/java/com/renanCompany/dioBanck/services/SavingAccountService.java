86096a0340cad9458e196508ea126a3233b8bb58
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 06:03:07 -0300

    criation of methods class SavingAccountService

--- src/main/java/com/renanCompany/dioBanck/services/SavingAccountService.java
+++ src/main/java/com/renanCompany/dioBanck/services/SavingAccountService.java
@@ -1,9 +1,49 @@
 package com.renanCompany.dioBanck.services;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
+
+import com.renanCompany.dioBanck.entities.Account;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class SavingAccountService {
+	
+	Account account = new Account();
+
+	@Transactional(rollbackOn = Exception.class)
+	public void deposit(BigDecimal value) {
+		if (value.compareTo(BigDecimal.ZERO) > 0) {
+			account.setBalance(account.getBalance().add(value));
+			System.out.println("Deposit succeeded!");
+		}
+		throw new RuntimeException("Deposit not succeeded! Value must be greater than zero.");
+	}
+
+	@Transactional(rollbackOn = Exception.class)
+	public void withdraw(BigDecimal value) {
+		if (value.compareTo(account.getBalance()) <= 0) {
+			account.setBalance(account.getBalance().subtract(value));
+			System.out.println("withdraw succeeded!");
+		}
+		throw new RuntimeException("withdraw not succeeded! no balance enough.");
+	}
+
+	@Transactional
+	public void transfer(Account payee, BigDecimal value) {
+		if (value.compareTo(BigDecimal.ZERO) > 0 && account.getBalance().compareTo(value) >= 0) {
+			account.setBalance(account.getBalance().subtract(value));
+
+			payee.setBalance(payee.getBalance().add(value));
+
+			System.out.println("Transfer succeeded!");
+		} else {
+			throw new RuntimeException("Transfer not succeeded! No balance enough.");
+
+		}
+	}
 	
 
 }
