f545a4551b1e8c6dde3382da91cb131869e375ed
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 07:14:07 -0300

    Creation of method delete in AccountController

--- src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
+++ src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,12 @@ public class AccountController {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		}
 		return ResponseEntity.status(HttpStatus.FOUND).body(optAccount);
+	}
+	
+	@DeleteMapping("/id")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		accountService.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
