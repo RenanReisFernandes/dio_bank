a2a019dcb332a58ff9230a1a82b0cd1058c7fcfe
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 05:36:28 -0300

    criation of method deposit in CheckingAccountService

--- src/main/java/com/renanCompany/dioBanck/services/CheckingAccountService.java
+++ src/main/java/com/renanCompany/dioBanck/services/CheckingAccountService.java
@@ -1,8 +1,25 @@
 package com.renanCompany.dioBanck.services;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
+
+import com.renanCompany.dioBanck.entities.Account;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CheckingAccountService {
+	
+	Account account = new Account();
+	
+	@Transactional(rollbackOn = Exception.class)
+	private void deposit(BigDecimal value) {
+		if(value.compareTo(BigDecimal.ZERO) > 0) {
+			account.setBalance(account.getBalance().add(value));
+			System.out.println("Deposit succeeded!");
+		}
+		 throw new RuntimeException("Deposit not succeeded! Value must be greater than zero.");
+	}
 
 }
