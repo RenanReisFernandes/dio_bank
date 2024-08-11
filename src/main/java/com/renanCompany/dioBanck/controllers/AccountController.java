ff2413773652593faa47853b40653fc312b5391e
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 11:31:46 -0300

    criation of docummentation swagger in account

--- src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
+++ src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
@@ -17,12 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.renanCompany.dioBanck.entities.Account;
 import com.renanCompany.dioBanck.services.AccountService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/account")
+@Tag(name = "API DIO BANK / ACCOUNT")
 public class AccountController {
 	@Autowired
 	private AccountService accountService;
 	
+	@Operation(summary = "criation of account", method = "POST")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "insertions succssed"),
+			@ApiResponse(responseCode = "422", description = "Invalid requirement"),
+			@ApiResponse(responseCode = "400", description = "Invalid params"),
+			@ApiResponse(responseCode = "401", description = "user not found"),
+			@ApiResponse(responseCode = "500", description = "server broke")
+			
+	})	
 	@PostMapping
 	public ResponseEntity<Account> save(@RequestBody Account account){
 		Account accountSaved = accountService.save(account);
