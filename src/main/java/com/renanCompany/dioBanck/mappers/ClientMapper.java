a2b23bcd49667cc18236aabc1595feb0fe8a7dc1
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 09:36:33 -0300

    Creation of class ClientMapper

---
+++ src/main/java/com/renanCompany/dioBanck/mappers/ClientMapper.java
@@ -0,0 +1,11 @@
+package com.renanCompany.dioBanck.mappers;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ClientMapper {
+
+}
