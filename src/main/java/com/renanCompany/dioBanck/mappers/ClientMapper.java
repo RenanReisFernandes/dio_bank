2093960767a13bdbdf9a8b6ecedc0b46a70e823b
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 09:43:08 -0300

    Creation of methods to dto

--- src/main/java/com/renanCompany/dioBanck/mappers/ClientMapper.java
+++ src/main/java/com/renanCompany/dioBanck/mappers/ClientMapper.java
@@ -1,11 +1,32 @@
 package com.renanCompany.dioBanck.mappers;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import com.renanCompany.dioBanck.DTO.request.ClientRequest;
+import com.renanCompany.dioBanck.DTO.response.ClientResponse;
+import com.renanCompany.dioBanck.entities.Client;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class ClientMapper {
+	
+	private final ModelMapper mapper;
+	
+	public Client toClient(ClientRequest clientRequest) {
+		return null;
+	}
+	
+	public ClientResponse toClientResponse(Client client) {
+		return null;
+	}
+	
+	public List<ClientResponse> toClientResponseList(List<Client> clientList){
+		return null;
+	}
 
 }
