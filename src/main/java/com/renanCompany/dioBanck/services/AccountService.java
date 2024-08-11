42cc5f862890bd9900db1477d6c93e7c343de3ad
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 05:14:59 -0300

    criation of method of service to delete account by id

--- src/main/java/com/renanCompany/dioBanck/services/AccountService.java
+++ src/main/java/com/renanCompany/dioBanck/services/AccountService.java
@@ -12,38 +12,40 @@ import com.renanCompany.dioBanck.repositories.AccountRepository;
 
 @Service
 public class AccountService {
-	
+
 	@Autowired
 	private AccountRepository accountRepository;
-	
+
 	public Account save(Account account) {
 		return accountRepository.save(account);
 	}
-	
-	public List<Account> findAll(){
+
+	public List<Account> findAll() {
 		List<Account> existentAccounts = accountRepository.findAll();
-		if(existentAccounts == null || existentAccounts.isEmpty()) {
-		 throw new NoSuchElementException("No account found!");
+		if (existentAccounts == null || existentAccounts.isEmpty()) {
+			throw new NoSuchElementException("No account found!");
 		}
-		
 		return existentAccounts;
-		
 	}
-	
-	public Optional<Account> findById(Long id){
+
+	public Optional<Account> findById(Long id) {
 		Optional<Account> optAccount = accountRepository.findById(id);
-		if(optAccount.isEmpty()) {
+		if (optAccount.isEmpty()) {
 			throw new NoSuchElementException("No account found!");
 		}
 		return optAccount;
 	}
-	
-	public Optional<Account> findByAccountNumber(Long accountNumber){
+
+	public Optional<Account> findByAccountNumber(Long accountNumber) {
 		Optional<Account> optAcount = accountRepository.findByaccountNumber(accountNumber);
-		if(optAcount.isEmpty()) {
+		if (optAcount.isEmpty()) {
 			throw new NoSuchElementException("Account not found!");
 		}
 		return optAcount;
 	}
-
+	
+	public void delete(Long id) {
+		accountRepository.deleteById(id);
+	}
+	
 }
