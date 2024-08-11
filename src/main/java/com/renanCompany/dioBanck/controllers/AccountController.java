712ffcaeadab99a47fff85aa0c3e3d61fe9e6132
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 06:50:48 -0300

    Creation of method save in AccountController

--- src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
+++ src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
@@ -1,10 +1,26 @@
 package com.renanCompany.dioBanck.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.renanCompany.dioBanck.entities.Account;
+import com.renanCompany.dioBanck.services.AccountService;
 
 @RestController
 @RequestMapping(value = "/account")
 public class AccountController {
+	@Autowired
+	private AccountService accountService;
+	
+	@PostMapping
+	public ResponseEntity<Account> save(@RequestBody Account account){
+		Account accountSaved = accountService.save(account);
+		return ResponseEntity.status(HttpStatus.CREATED).body(accountSaved);
+	}
 
 }
