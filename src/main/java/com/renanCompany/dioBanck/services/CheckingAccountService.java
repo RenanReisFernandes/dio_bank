8082cc02ad9249bd722b64acbf336bfef30b81f5
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 05:57:17 -0300

    criation of method transfer in CheckingAccountService

--- src/main/java/com/renanCompany/dioBanck/services/CheckingAccountService.java
+++ src/main/java/com/renanCompany/dioBanck/services/CheckingAccountService.java
@@ -10,25 +10,39 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class CheckingAccountService {
-	
+
 	Account account = new Account();
-	
+
 	@Transactional(rollbackOn = Exception.class)
-	private void deposit(BigDecimal value) {
-		if(value.compareTo(BigDecimal.ZERO) > 0) {
+	public void deposit(BigDecimal value) {
+		if (value.compareTo(BigDecimal.ZERO) > 0) {
 			account.setBalance(account.getBalance().add(value));
 			System.out.println("Deposit succeeded!");
 		}
-		 throw new RuntimeException("Deposit not succeeded! Value must be greater than zero.");
+		throw new RuntimeException("Deposit not succeeded! Value must be greater than zero.");
 	}
-	
+
 	@Transactional(rollbackOn = Exception.class)
-	private void withdraw(BigDecimal value) {
-		if(value.compareTo(account.getBalance()) <= 0) {
+	public void withdraw(BigDecimal value) {
+		if (value.compareTo(account.getBalance()) <= 0) {
 			account.setBalance(account.getBalance().subtract(value));
 			System.out.println("withdraw succeeded!");
 		}
-		 throw new RuntimeException("withdraw not succeeded! no balance enough.");
+		throw new RuntimeException("withdraw not succeeded! no balance enough.");
+	}
+
+	@Transactional
+	public void transfer(Account payee, BigDecimal value) {
+		if (value.compareTo(BigDecimal.ZERO) > 0 && account.getBalance().compareTo(value) >= 0) {
+			account.setBalance(account.getBalance().subtract(value));
+
+			payee.setBalance(payee.getBalance().add(value));
+
+			System.out.println("Transfer succeeded!");
+		} else {
+			throw new RuntimeException("Transfer not succeeded! No balance enough.");
+
+		}
 	}
 
 }
