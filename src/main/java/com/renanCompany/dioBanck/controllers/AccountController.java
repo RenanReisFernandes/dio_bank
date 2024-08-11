0d272e4d8e97432906ff6ea08976b59f373cd73a
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-11 06:53:15 -0300

    Creation of method findAll in AccountController

--- src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
+++ src/main/java/com/renanCompany/dioBanck/controllers/AccountController.java
@@ -1,8 +1,11 @@
 package com.renanCompany.dioBanck.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,12 @@ public class AccountController {
 	public ResponseEntity<Account> save(@RequestBody Account account){
 		Account accountSaved = accountService.save(account);
 		return ResponseEntity.status(HttpStatus.CREATED).body(accountSaved);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Account>> findAll(){
+		List<Account> list = accountService.findAll();
+		return ResponseEntity.status(HttpStatus.FOUND).body(list);
 	}
 
 }
