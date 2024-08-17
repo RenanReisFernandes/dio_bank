ff096adc541fd69d3836b5115f631692a0e9e6a0
Renan Reis <renanmacfernandes@hotmail.com>
2024-08-17 17:23:59 -0300

    criation of service for authorizstion

--- src/main/java/com/renanCompany/dioBanck/security/repositories/UserRepository.java
+++ src/main/java/com/renanCompany/dioBanck/security/repositories/UserRepository.java
@@ -3,9 +3,12 @@ package com.renanCompany.dioBanck.security.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import com.renanCompany.dioBanck.security.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+	UserDetails findByLogin(String login);
 
 }
